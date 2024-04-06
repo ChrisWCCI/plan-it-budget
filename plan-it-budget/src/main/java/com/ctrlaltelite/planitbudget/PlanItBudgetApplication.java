2d3b3b45db2ca2b4b4d45e4ea5e58dd8ababe5f4
ProgramWithGraham <gaps512@gmail.com>
2024-04-06 15:33:33 -0500

    updte

---
+++ plan-it-budget/src/main/java/com/ctrlaltelite/planitbudget/PlanItBudgetApplication.java
@@ -0,0 +1,13 @@
+package com.ctrlaltelite.planitbudget;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanItBudgetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanItBudgetApplication.class, args);
+	}
+
+}
---
+++ plan-it-budget/src/test/java/com/ctrlaltelite/planitbudget/PlanItBudgetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ctrlaltelite.planitbudget;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlanItBudgetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
