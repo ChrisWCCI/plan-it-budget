b58e8c93f7eb61838a4ba94a78e12ea7b5a03512
ProgramWithGraham <gaps512@gmail.com>
2024-04-07 13:01:37 -0500

    added starter code in all Entities

--- backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Debts.java
+++ backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Debts.java
@@ -1,5 +1,26 @@
 package com.ctrlaltelite.planitbudget.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"debts\"")
+
 public class Debts {
+    public Debts() {
+    }
+
+    public Debts(Long money) {
+
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private long id;
 
 }
