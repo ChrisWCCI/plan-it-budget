9f7c6575966381968814dcce5cc5cdc41988e94e
ProgramWithGraham <gaps512@gmail.com>
2024-04-09 18:42:46 -0500

    updating back files for login

--- backend/src/main/java/com/ctrlaltelite/planitbudget/service/UserService.java
+++ backend/src/main/java/com/ctrlaltelite/planitbudget/service/UserService.java
@@ -1,12 +1,11 @@
 package com.ctrlaltelite.planitbudget.service;
 
-
 import com.ctrlaltelite.planitbudget.web.dto.UserDto;
 import com.ctrlaltelite.planitbudget.web.dto.LoginDto;
 import com.ctrlaltelite.planitbudget.web.dto.LoginMessage;
 
-public interface UserService{
-    String addUser(UserDto userRegistrationDto);
+public interface UserService {
+    String addUser(UserDto userDto);
 
     LoginMessage loginUser(LoginDto loginDto);
 }
