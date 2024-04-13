5ebc3bc754d26c51102b51d1995c6ec076d41da1
ProgramWithGraham <gaps512@gmail.com>
2024-04-13 09:08:13 -0500

    u

--- backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Debts.java
+++ backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Debts.java
@@ -6,6 +6,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.Table;
+
 
 @Entity
 @Table(name = "\"debts\"")
@@ -22,6 +24,24 @@ public class Debts {
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private long id;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 }
