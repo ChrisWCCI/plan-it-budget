f8d80033a26292010514ffabc1d4105a4f7e08a2
ProgramWithGraham <gaps512@gmail.com>
2024-04-13 14:37:33 -0500

    updated expense

--- backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Expenses.java
+++ backend/src/main/java/com/ctrlaltelite/planitbudget/entity/Expenses.java
@@ -1,11 +1,7 @@
 package com.ctrlaltelite.planitbudget.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "\"expenses\"")
@@ -14,13 +10,56 @@ public class Expenses {
     public Expenses() {
     }
 
-    public Expenses(Long money) {
-
+    public Expenses(LocalDate transactionDate, String expenseName, Double chargeAmount) {
+        this.transactionDate = transactionDate;
+        this.expenseName = expenseName;
+        this.chargeAmount = chargeAmount;
     }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     @Column(name = "id")
     private long id;
+
+    @Column(name = "transactionDate", nullable = false)
+    private LocalDate transactionDate;
+
+    @Column(name = "expenseName", length = 700, nullable = false)
+    private String expenseName;
+
+    @Column(name = "chargeAmount", nullable = true)
+    private Double chargeAmount;
+
+    public long getId() {
+        return this.id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public LocalDate getTransactionDate() {
+        return this.transactionDate;
+    }
+
+    public void setTransactionDate(LocalDate transactionDate) {
+        this.transactionDate = transactionDate;
+    }
+
+    public String getExpenseName() {
+        return this.expenseName;
+    }
+
+    public void setExpenseName(String expenseName) {
+        this.expenseName = expenseName;
+    }
+
+    public Double getChargeAmount() {
+        return this.chargeAmount;
+    }
+
+    public void setChargeAmount(Double chargeAmount) {
+        this.chargeAmount = chargeAmount;
+    }
 
 }
--- backend/src/main/java/com/ctrlaltelite/planitbudget/repository/ExpensesRepository.java
+++ backend/src/main/java/com/ctrlaltelite/planitbudget/repository/ExpensesRepository.java
@@ -2,10 +2,17 @@ package com.ctrlaltelite.planitbudget.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.time.LocalDate;
 
 import com.ctrlaltelite.planitbudget.entity.Expenses;
 
 @Repository
 public interface ExpensesRepository extends JpaRepository<Expenses, Long> {
+
+    Iterable<Expenses> findByTransactionDate(LocalDate transactionDate);
+
+    Iterable<Expenses> findByExpenseName(String expenseName);
+
+    Iterable<Expenses> findByChargeAmount(Double chargeAmount);
 
 }
