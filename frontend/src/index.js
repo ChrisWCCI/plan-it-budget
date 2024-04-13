d0f025124f9e920dd4db1c38a20166c04cdb293d
tcollert <tcollert1@gmail.com>
2024-04-13 17:02:52 -0400

    changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,21 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BudgetsProvider } from "./contexts/BudgetsContext";
 
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BudgetsProvider>
       <App />
-      
     </BudgetsProvider>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/pages/DebtBurnDown.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function DebtBurnDown() {
+  return (
+    <div className='retirement'>
+      <h1>Debt Burn Down</h1>
+    </div>
+  );
+}
+
+export default DebtBurnDown;
