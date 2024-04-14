4410b66633b34824b67b0f4ab728c7e317118a30
tlc67 <kris7cuevas@gmail.com>
2024-04-14 08:57:09 -0400

    updates

--- frontend/src/components/SidebarData.js
+++ frontend/src/components/SidebarData.js
@@ -1,40 +1,48 @@
-import React from 'react';
-import { FaHouse, FaMoneyBillTrendUp, FaFireFlameCurved, FaMountainSun, FaSackDollar, FaUsersViewfinder  } from "react-icons/fa6";
+import React from "react";
+import {
+  FaHouse,
+  FaMoneyBillTrendUp,
+  FaFireFlameCurved,
+  FaMountainSun,
+  FaSackDollar,
+  FaUsersViewfinder,
+} from "react-icons/fa6";
 
-export const SidebarData =[
-    
-{
-    title: 'Home',
-    path: '/',
+export const SidebarData = [
+  {
+    title: "Home",
+    path: "/",
     icon: <FaHouse />,
-    cName: 'nav-text',
-},
-{
-    title: 'Goals',
-    path: '/Goals',
+    cName: "nav-text",
+  },
+  {
+    title: "Goals",
+    path: "/Goals",
     icon: <FaMoneyBillTrendUp />,
-    cName: 'nav-text',
-},
-{
-    title: 'Burn Down',
-    path: '/Burn Down',
+    cName: "nav-text",
+  },
+  {
+    title: "Burn Down",
+    path: "/Burn Down",
     icon: <FaFireFlameCurved />,
-    cName: 'nav-text',
-},{
-    title: 'Retirement',
-    path: '/Retirement',
+    cName: "nav-text",
+  },
+  {
+    title: "Retirement",
+    path: "/Retirement",
     icon: <FaMountainSun />,
-    cName: 'nav-text',
-},{
-    title: 'Resources',
-    path: '/Resources',
+    cName: "nav-text",
+  },
+  {
+    title: "Resources",
+    path: "/Resources",
     icon: <FaSackDollar />,
-    cName: 'nav-text',
-},{
-    title: 'About',
-    path: '/About',
+    cName: "nav-text",
+  },
+  {
+    title: "About",
+    path: "/About",
     icon: <FaUsersViewfinder />,
-    cName: 'nav-text',
-},
-
-]
+    cName: "nav-text",
+  },
+];
